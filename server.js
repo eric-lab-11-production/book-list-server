c64ccc7294439e03c7d03e63e80f6867bea78a67
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-04-14 15:54:41 -0700

    Added cors library

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 const pg = require('pg');
+const cors = require('cors');
 const PORT = process.env.PORT;
 const app = express();
 
@@ -10,10 +11,12 @@ const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
 const client = new pg.Client(DATABASE_URL);
 client.connect();
+client.on('error', err => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(CLIENT_URL));
+app.use(cors());
+//app.use(express.static('../book-list-client));
 
 app.get('/test', (req,res) => {
   client.query(`
@@ -23,4 +26,5 @@ app.get('/test', (req,res) => {
     .catch(console.error);
 });
 
+app.get('*',(req,res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Active on port ${PORT}`));
