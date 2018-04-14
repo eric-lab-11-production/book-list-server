eb8a2979e8a80f5d0eb9f9d81f32bc3aeb1a040c
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-04-14 14:04:58 -0700

    Set up local database

--- server.js
+++ server.js
@@ -2,16 +2,22 @@
 'use strict';
 
 const express = require('express');
-//const pg = require('pg');
+const pg = require('pg');
 const PORT = process.env.PORT;
 const app = express();
 
-// const db_URL = process.env.db_URL;
-// const client = new pg.Client(db_URL);
+const db_URL = process.env.db_URL;
+const client = new pg.Client(db_URL);
+client.connect();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-//app.use(express.static, ('./book-list-client'));
+app.use(express.urlencoded({ extended: true }));
+//app.use(express.static, ('../book-list-client'));
 
-app.get('/test', (req,res) => res.send('hello world'));
+app.get('/test', (req,res) => {
+  client.query(`
+        SELECT author FROM books;
+        `).then(result => res.send(result.rows));
+});
+
 app.listen(PORT, () => console.log(`Active on port ${PORT}`));
