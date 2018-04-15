d4900918d349c37af3c7256884099bec735247d4
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-04-14 18:13:23 -0700

    testing cors

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const PORT = process.env.PORT;
 const app = express();
 
+const allowed_url = [
+  'http://localhost:3000/',
+  'https://eric-lab-11-production.github.io/book-list-client/',
+];
+
 const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
 const client = new pg.Client(DATABASE_URL);
@@ -15,8 +20,14 @@ client.on('error', err => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(cors());
-app.use(express.static(CLIENT_URL));
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null,true);
+    if(allowed_url.indexOf(origin)===-1) return callback('No', false);
+    return callback(null,true);
+  }
+}));
+// app.use(express.static(CLIENT_URL));
 
 app.get('/books', (req,res) => {
   client.query(`
@@ -26,5 +37,5 @@ app.get('/books', (req,res) => {
     .catch(console.error);
 });
 
-//app.get('*',(req,res) => res.redirect(CLIENT_URL));
+app.get('*',(req,res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Active on port ${PORT}`));
