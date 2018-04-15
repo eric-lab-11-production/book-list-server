5aac227c12e4fb9f15b43e197560234288b4bbbe
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-04-14 23:29:40 -0700

    Cors test

--- server.js
+++ server.js
@@ -19,21 +19,29 @@ const allowed_url = [
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-// app.use(cors({
-//   origin: function(origin, callback){
-//     if(!origin) return callback(null,true);
-//     if(allowed_url.indexOf(origin)===-1) return callback('No', false);
-//     return callback(null,true);
-//   },
-//   credentials: true,
-// }));
+// app.use(cors());
+app.use(cors({
+  origin: function(origin, callback){
+    if(!origin) return callback(null,true);
+    if(allowed_url.indexOf(origin)===-1) return callback('No', false);
+    return callback(null,true);
+  },
+  credentials: true,
+}));
 
-// app.use(express.static('../book-list-client'));
+// app.use((req,res,next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//   res.setHeader('Access-Control-Allow-Credentials',true);
+//   next();
+// });
+
+//app.use(express.static('../book-list-client'));
 app.get('/', (req,res) => {
   res.sendFile('index.html', {root: '../book-list-client'});
 });
-app.get('/', (req,res) => res.send('I AM ALIVE'));
+//app.get('/', (req,res) => res.send('I AM ALIVE'));
 
 app.get('/books', (req,res) => {
   client.query(`
@@ -43,5 +51,5 @@ app.get('/books', (req,res) => {
     .catch(console.error);
 });
 
-app.get('*',(req,res) => res.redirect(CLIENT_URL));
+app.get('/',(req,res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Active on port ${PORT}`));
