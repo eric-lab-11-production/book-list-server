f6c0f8a193d237ac14678cacb379439ee45139ae
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-04-14 21:42:58 -0700

    Cors test

--- server.js
+++ server.js
@@ -4,34 +4,36 @@
 const express = require('express');
 const pg = require('pg');
 const cors = require('cors');
-const PORT = process.env.PORT;
 const app = express();
+const PORT = process.env.PORT;
 
-const allowed_url = [
-  'http://localhost:3000/book-list-client',
-  'https://eric-lab-11-production.github.io/book-list-client/',
-];
-
-const DATABASE_URL = process.env.DATABASE_URL;
 const CLIENT_URL = process.env.CLIENT_URL;
-const client = new pg.Client(DATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.log(err));
 
+const allowed_url = [
+  'http://localhost:8080',
+  'https://eric-lab-11-production.github.io/book-list-client/',
+];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null,true);
-    if(allowed_url.indexOf(origin)===-1) return callback('No', false);
-    return callback(null,true);
-  }
-}));
-app.use(express.static(CLIENT_URL));
+app.use(cors());
+// app.use(cors({
+//   origin: function(origin, callback){
+//     if(!origin) return callback(null,true);
+//     if(allowed_url.indexOf(origin)===-1) return callback('No', false);
+//     return callback(null,true);
+//   },
+//   credentials: true,
+// }));
 
+// app.use(express.static('../book-list-client'));
 app.get('/', (req,res) => {
-  res.sendFile('index.html', {root: '../book-list-client/'});
+  res.sendFile('index.html', {root: '../book-list-client'});
 });
+app.get('/', (req,res) => res.send('I AM ALIVE'));
 
 app.get('/books', (req,res) => {
   client.query(`
@@ -40,6 +42,6 @@ app.get('/books', (req,res) => {
     `).then(result => res.send(result.rows))
     .catch(console.error);
 });
-//console.log(CLIENT_URL);
+
 app.get('*',(req,res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Active on port ${PORT}`));
